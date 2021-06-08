7a4756b916999676bc1efedbd89d902536fa8119
TristanLieberherr <tristan.lieberherr@gmail.com>
2021-06-08 11:26:33 +0200

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,33 +7,39 @@ use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-        User::insert([
-          'name' => 'Lieberherr',
-          'surname' => 'Tristan',
-          'email' => 'tristan',
-          'password' => bcrypt('password'),
-      ]);
-        User::insert([
-          'name' => 'Bravo',
-          'surname' => 'Johnny',
-          'email' => 'tech',
-          'is_technician' => true,
-          'password' => bcrypt('password'),
-      ]);
-      User::insert([
-        'name' => 'Ventura',
-        'surname' => 'Ace',
-        'email' => 'tech2',
-        'is_technician' => true,
-        'password' => bcrypt('password'),
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // \App\Models\User::factory(10)->create();
+    User::insert([
+      'name' => 'Lieberherr',
+      'surname' => 'Tristan',
+      'email' => 'tristan',
+      'password' => bcrypt('password'),
     ]);
-    }
+    User::insert([
+      'name' => 'Chevallier',
+      'surname' => 'Yves',
+      'email' => 'yves',
+      'password' => bcrypt('password'),
+    ]);
+      User::insert([
+      'name' => '1',
+      'surname' => 'Tech',
+      'email' => 'tech',
+      'is_technician' => true,
+      'password' => bcrypt('password'),
+    ]);
+    User::insert([
+      'name' => '2',
+      'surname' => 'Tech',
+      'email' => 'tech2',
+      'is_technician' => true,
+      'password' => bcrypt('password'),
+  ]);
+  }
 }
